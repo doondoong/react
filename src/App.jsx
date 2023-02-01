ce253ba6fd446cbce0c587257dbbc390c4ee0578
doondoong <pztdrd@nate.com>
2023-02-01 12:01:24 +0900

    api 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
 import React,{useEffect, useState} from 'react';
 
 export default function App() {
+    const [data, setData ] = useState()
+    const url = 'https://proxy.cors.sh/https://openapi.gg.go.kr/Kidscafe?Key=5bd2875e523143338161caeb75b2f4d5&pindex=1&pSize=50&SIGUN_NM=남양주시'
+    async function postData(url = '') {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'x-cors-api-key': 'temp_2f26b6f293511613cad7af0618d3f4dc',
+                "Content-Type": "application/json",
+            },
+        });
+        return response.json()
+    }
+    useEffect(()=>{
+        postData(url).then((data) => {
+            console.log(data,'firstData')
+            setData(data.Kidscafe[1].row)
+        })
+    },[])
 
-    fetch("https://proxy.cors.sh/https://openapi.gg.go.kr/Kidscafe?Key=5bd2875e523143338161caeb75b2f4d5&pindex=1&pSize=50&SIGUN_NM=남양주시",{
-        method: "POST",
-        headers: {
-            'x-cors-api-key': 'temp_2f26b6f293511613cad7af0618d3f4dc',
-            "Content-Type": "application/json",
-        },
-        // body: JSON.stringify({
-        //     Key: "5bd2875e523143338161caeb75b2f4d5",
-        //     Type: "JSON",
-        //     pindex: 1,
-        //     pSize: 50,
-        //     SIGUN_NM: "남양주시"
-        // }),
-    }).then((response) => console.log(response.json()))
-
+    console.log(data ,'data')
     return (
         <div>
-            안녕하세요
+            <table border="1">
+                <thead>
+                </thead>
+                <th>업체명</th>
+                <th>주소</th>
+                <th>오픈일</th>
+                <tbody>
+                {data?.map((item, index) => {
+                    return (
+                    <tr>
+                        <td>{item.BIZPLC_NM}</td>
+                        <td>{item.REFINE_LOTNO_ADDR}</td>
+                        <td>{item.LICENSG_DE}</td> 
+                    </tr>)
+                })} 
+                </tbody>
+            </table>
         </div>
     );
 }
