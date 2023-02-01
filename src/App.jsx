94505730fe692d661b5a329bc2566ba4ac95a688
doondoong <pztdrd@nate.com>
2023-02-01 10:43:20 +0900

    edit code

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React,{useEffect, useState} from 'react';
 
 export default function App() {
 
-    fetch("https://proxy.cors.sh/https://openapi.gg.go.kr/Kidscafe",{
+    fetch("https://proxy.cors.sh/https://openapi.gg.go.kr/Kidscafe?Key=5bd2875e523143338161caeb75b2f4d5&Type=JSON&pindex=1&pSize=50&SIGUN_NM=남양주시",{
         method: "POST",
         headers: {
             'x-cors-api-key': 'temp_2f26b6f293511613cad7af0618d3f4dc',
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-            Key: "5bd2875e523143338161caeb75b2f4d5",
-            Type: "JSON",
-            pindex: 1,
-            pSize: 50,
-            SIGUN_NM: "남양주시"
-        }),
+        // body: JSON.stringify({
+        //     Key: "5bd2875e523143338161caeb75b2f4d5",
+        //     Type: "JSON",
+        //     pindex: 1,
+        //     pSize: 50,
+        //     SIGUN_NM: "남양주시"
+        // }),
     }).then((response) => console.log(response))
 
     return (
