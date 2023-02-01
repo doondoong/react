1741a64b29c373373e9442e74857d5236ee720b5
doondoong <pztdrd@nate.com>
2023-02-01 09:41:35 +0900

    edit code

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,21 @@ import React,{useEffect, useState} from 'react';
 
 export default function App() {
     const url = 'https://openapi.gg.go.kr/Kidscafe'
-    const prams = {
-        Key: "5bd2875e523143338161caeb75b2f4d5",
-        Type: "JSON",
-        pindex: 1,
-        pSize: 50,
-        SIGUN_NM: "남양주시"
-    }
-    useEffect(async ()=>{
-        let response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                Authentication: '5bd2875e523143338161caeb75b2f4d5'
-            },
-            body: JSON.stringify()
-        });
-        if(response.ok) {
-            let json = await response.json()
-            console.log(json,'json')
-        } else {
-            alert("HTTP-Error: " + response.status);
-        }
-    },[])
+
+    fetch(url,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            Key: "5bd2875e523143338161caeb75b2f4d5",
+            Type: "JSON",
+            pindex: 1,
+            pSize: 50,
+            SIGUN_NM: "남양주시"
+        }),
+    }).then((response) => console.log(response))
+
     return (
         <div>
             안녕하세요
