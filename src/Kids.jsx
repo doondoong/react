fcedb0fbeeddd19d1c253e34d031f64a359c62fa
doondoong <pztdrd@nate.com>
2023-02-02 18:46:39 +0900

    박스 css 수정

--- src/Kids.jsx
+++ src/Kids.jsx
@@ -11,28 +11,30 @@ export default function Kids() {
     return !loading ? (
         <div>
             <SelectBox sigun_nm={sigun_nm} setSigun_nm={setSigun_nm} />
-            <table className='border-collapse border border-slate-400 mt-2'>
-                <thead>
-                    <th className='border border-slate-300'>업체명</th>
-                    <th className='border border-slate-300'>주소</th>
-                    <th className='border border-slate-300'>오픈일</th>
-                    <th className='border border-slate-300'>영업상태</th>
-                </thead>
-                <tbody>
-                {data?.map((item , index) => {
-                    if(item.BSN_STATE_NM === '폐업') {
-                        return 
-                    }
-                    return (
-                    <tr key={index}>
-                        <td className='border border-slate-300'>{item.BIZPLC_NM}</td>
-                        <td className='border border-slate-300'>{item.REFINE_LOTNO_ADDR}</td>
-                        <td className='border border-slate-300'>{item.LICENSG_DE}</td> 
-                        <td className='border border-slate-300'>{item.BSN_STATE_NM}</td> 
-                    </tr>)
-                })} 
-                </tbody>
-            </table>
+            <div className='flex justify-center'>
+                <table className='border border-slate-400 mt-2 w-full mx-2'>
+                    <thead className='bg-gray-400'>
+                        <th className='border border-slate-600'>업체명</th>
+                        <th className='border border-slate-600'>주소</th>
+                        <th className='border border-slate-600'>오픈일</th>
+                        <th className='border border-slate-600'>영업상태</th>
+                    </thead>
+                    <tbody>
+                    {data?.map((item , index) => {
+                        if(item.BSN_STATE_NM === '폐업') {
+                            return 
+                        }
+                        return (
+                        <tr key={index}>
+                            <td className='border border-slate-600'>{item.BIZPLC_NM}</td>
+                            <td className='border border-slate-600'>{item.REFINE_LOTNO_ADDR}</td>
+                            <td className='border border-slate-600'>{item.LICENSG_DE}</td> 
+                            <td className='border border-slate-600'>{item.BSN_STATE_NM}</td> 
+                        </tr>)
+                    })} 
+                    </tbody>
+                </table>
+            </div>
         </div>
     ) : <Loading />
 }
