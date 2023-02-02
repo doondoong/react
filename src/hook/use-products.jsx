eec9a8461a5401c4ee43529870c8efdcc35c1266
doondoong <pztdrd@nate.com>
2023-02-02 18:10:45 +0900

    .env테스트2

--- src/hook/use-products.jsx
+++ src/hook/use-products.jsx
@@ -1,15 +1,17 @@
 import {useState, useEffect} from "react";
+import { envVars } from "../vars/envVars";
 
 export default function useProducts({sigun_nm}) {
     const [data, setData] = useState()
     const [error, setError] = useState()
     const [loading, setLoading] = useState(false)
-    
+    const { API_KEY, API_KIDS_KEY } = envVars
+
     async function postData(url = '') {
         const response = await fetch(url, {
             method: "POST",
             headers: {
-                'x-cors-api-key': 'temp_2f26b6f293511613cad7af0618d3f4dc',
+                'x-cors-api-key': API_KEY,
                 "Content-Type": "application/json",
             },
         });
@@ -18,7 +20,7 @@ export default function useProducts({sigun_nm}) {
 
     useEffect(()=>{
         setLoading(true)
-        const url = `https://proxy.cors.sh/https://openapi.gg.go.kr/Kidscafe?Key=5bd2875e523143338161caeb75b2f4d5&pindex=1&SIGUN_NM=${sigun_nm}&pSize=100`
+        const url = `https://proxy.cors.sh/https://openapi.gg.go.kr/Kidscafe?Key=${API_KIDS_KEY}&pindex=1&SIGUN_NM=${sigun_nm}&pSize=100`
         postData(url).then((data) => {
             setData(data.Kidscafe[1].row)
         }).catch((e) => {
--- src/vars/envVars.js
+++ src/vars/envVars.js
@@ -1,5 +1,6 @@
 const envVars = {
-    cAPI_KEY: process.env.REACT_APP_API_KEY
+    API_KEY: process.env.REACT_APP_API_KEY,
+    API_KIDS_KEY: process.env.REACT_APP_API_KEDS_KEY
 }
 
 
