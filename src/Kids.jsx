4d3798eb819e1e20b9fadc70af191d76b14be103
doondoong <pztdrd@nate.com>
2023-02-02 18:38:47 +0900

    셀렉트 박스 변경

--- src/Kids.jsx
+++ src/Kids.jsx
@@ -11,13 +11,13 @@ export default function Kids() {
     return !loading ? (
         <div>
             <SelectBox sigun_nm={sigun_nm} setSigun_nm={setSigun_nm} />
-            <table className='border-2' style={{marginTop: '20px'}}>
+            <table className='border-collapse border border-slate-400 mt-2'>
                 <thead>
+                    <th className='border border-slate-300'>업체명</th>
+                    <th className='border border-slate-300'>주소</th>
+                    <th className='border border-slate-300'>오픈일</th>
+                    <th className='border border-slate-300'>영업상태</th>
                 </thead>
-                <th>업체명</th>
-                <th>주소</th>
-                <th>오픈일</th>
-                <th>영업상태</th>
                 <tbody>
                 {data?.map((item , index) => {
                     if(item.BSN_STATE_NM === '폐업') {
@@ -25,10 +25,10 @@ export default function Kids() {
                     }
                     return (
                     <tr key={index}>
-                        <td>{item.BIZPLC_NM}</td>
-                        <td>{item.REFINE_LOTNO_ADDR}</td>
-                        <td>{item.LICENSG_DE}</td> 
-                        <td>{item.BSN_STATE_NM}</td> 
+                        <td className='border border-slate-300'>{item.BIZPLC_NM}</td>
+                        <td className='border border-slate-300'>{item.REFINE_LOTNO_ADDR}</td>
+                        <td className='border border-slate-300'>{item.LICENSG_DE}</td> 
+                        <td className='border border-slate-300'>{item.BSN_STATE_NM}</td> 
                     </tr>)
                 })} 
                 </tbody>
--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -5,16 +5,19 @@ export default function SelectBox({sigun_nm, setSigun_nm}) {
     const handleChange = (e) => {
         setSigun_nm(e.target.value)
     }
+
     return (
-        <select 
-        onChange={handleChange} 
-        value={sigun_nm} 
-        name='choice' 
-        className='bg-sky-500 hover:bg-sky-700'
-        style={{width: '150px', height: '30px', marginTop: '10px', marginLeft: '10px'}}>
-            <option value="구리시 ">구리시</option>
-            <option value="남양주시">남양주시</option>
-        </select>
+        <div className='flex justify-end items-center mr-4 '>
+            <select 
+            onChange={handleChange} s
+            value={sigun_nm} 
+            name='choice' 
+            className='bg-sky-500 hover:bg-sky-700 rounded-lg'
+            style={{width: '150px', height: '30px', marginTop: '10px', marginLeft: '10px'}}>
+                <option value="구리시 ">구리시</option>
+                <option value="남양주시">남양주시</option>
+            </select>
+        </div>
     );
 }
 
