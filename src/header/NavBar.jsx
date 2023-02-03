b623e35897f255ea3297c3f616155df56c61e0f6
doondoong <pztdrd@nate.com>
2023-02-03 17:01:58 +0900

    이둥이사진추가

--- src/header/NavBar.jsx
+++ src/header/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled, {css} from 'styled-components';
 
+
 /** 네브바 */
 const NavWrapper = styled.div`
     display: flex;
@@ -22,7 +23,10 @@ export default function NavBar() {
     return (
         <NavWrapper>
             <div className='mt-2 h-2/3 flex flex-row justify-center'>
-                <button className='text-3xl text-rose-900 font-semibold hover:text-rose-600'>이둥이네 정보센터</button>
+                <img className='h-10 w-10 object-fill mr-22' src='doong.png' /> 
+                <Link to='/react' className='text-3xl text-rose-900 font-semibold hover:text-rose-600'>                
+                    이둥이네 정보센터
+                </Link>
             </div>
             <nav className='h-1/3 w-full flex flex-row items-center justify-start space-x-10 p-4'>
                 <Link to='/react' className='text-rose-900 hover:text-rose-600 font-semibold hover:origin-center hover:rotate-12'>키즈카페</Link>
---
+++ src/icon/Truck-plow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function TruckPlow() {
+    return (
+        <div>
+            <FontAwesomeIcon icon="fa-solid fa-truck-tow" />
+        </div>
+    );
+}
+
